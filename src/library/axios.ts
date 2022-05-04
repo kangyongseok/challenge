27cb5f66fb93e46d8d639f9c42e0b4ab2428aba9
Danah <danah.kim@kakao.com>
2022-05-04 12:50:08 +0900

    수정 axios 파일 클래스에서 함수로 변경

--- src/library/axios.ts
+++ src/library/axios.ts
@@ -1,36 +1,27 @@
-/* eslint @typescript-eslint/no-explicit-any: 'off' */
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
-class Axios {
-  private readonly axiosInstance: AxiosInstance;
+const axiosInstance = axios.create({ baseURL: process.env.API_BASE_URL });
 
-  constructor(config: AxiosRequestConfig) {
-    this.axiosInstance = axios.create(config);
-  }
-
+const axios = {
   setToken(token?: string) {
     if (token) {
-      this.axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+      axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
     } else {
-      delete this.axiosInstance.defaults.headers.common.Authorization;
+      delete axiosInstance.defaults.headers.common.Authorization;
     }
+  },
+  get<T>(url: string, config?: AxiosRequestConfig) {
+    return axiosInstance.get<T>(url, config);
+  },
+  post<T>(url: string, data: any, config?: AxiosRequestConfig) {
+    return axiosInstance.post<T>(url, data, config);
+  },
+  put<T>(url: string, data: any, config?: AxiosRequestConfig) {
+    return axiosInstance.put<T>(url, data, config);
+  },
+  delete<T>(url: string, config?: AxiosRequestConfig) {
+    return axiosInstance.delete<T>(url, config);
   }
+};
 
-  get<D>(url: string, config?: AxiosRequestConfig) {
-    return this.axiosInstance.get<D>(url, config);
-  }
-
-  post<D>(url: string, data: any, config?: AxiosRequestConfig) {
-    return this.axiosInstance.post<D>(url, data, config);
-  }
-
-  put<D>(url: string, data: any, config?: AxiosRequestConfig) {
-    return this.axiosInstance.put<D>(url, data, config);
-  }
-
-  delete<D>(url: string, config?: AxiosRequestConfig) {
-    return this.axiosInstance.delete<D>(url, config);
-  }
-}
-
-export default new Axios({ baseURL: process.env.API_BASE_URL });
+export default axios;
