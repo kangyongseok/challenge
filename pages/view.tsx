450c57d49010f07646888ec44e5f754da0d00f04
Noah <kangyongsuek@gmail.com>
2023-02-02 17:20:46 +0900

    [HOTFIX] CRM 간헐적 페이지 없음 오류 방어 리다이렉트 페이지 생성 (#592)
    - CRM 간헐적 페이지 없음 오류 방어 리다이렉트 페이지 생성

---
+++ pages/view.tsx
@@ -0,0 +1,21 @@
+import type { GetServerSidePropsContext } from 'next';
+
+function View() {
+  return <div>리다이렉트 페이지 이동중...</div>;
+}
+
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
+  // crm 시 앱에서 간헐적 리다이렉트 실패로 page notfound 오류 방어 코드
+  let result = `/${query.view}?`;
+  const excludeViewKeys = Object.keys(query).filter((queryKey) => queryKey !== 'view');
+
+  result += excludeViewKeys.map((key) => `${key}=${query[key]}`).join('&');
+  return {
+    redirect: {
+      destination: result,
+      permanent: false
+    }
+  };
+}
+
+export default View;
