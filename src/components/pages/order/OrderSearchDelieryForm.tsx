c054a03871aa6364041a9ad2e9d3d54468e0b1c8
Noah <kangyongsuek@gmail.com>
2023-05-24 17:58:50 +0900

    [HOTFIX] 배송조회 키값 api 호출 적용 (#809)
    * 보완 배송조회 키값 api 호출 적용
    
    * 보완 쿼리 캐시 초기화

--- src/components/pages/order/OrderSearchDelieryForm.tsx
+++ src/components/pages/order/OrderSearchDelieryForm.tsx
@@ -1,11 +1,13 @@
 import { FormEvent, useState } from 'react';
 
 import { useRouter } from 'next/router';
+import { find } from 'lodash-es';
 import { useQuery } from '@tanstack/react-query';
 import { Box, Button, Flexbox, Icon } from '@mrcamelhub/camel-ui';
 import { ReactJSXElement } from '@emotion/react/types/jsx-namespace';
 
 import { fetchOrder } from '@api/order';
+import { fetchCommonCodeDetails } from '@api/common';
 
 import queryKeys from '@constants/queryKeys';
 
@@ -24,6 +26,14 @@ function OrderSearchDelieryForm({
     () => fetchOrder(id || Number(query.id)),
     {
       enabled: !!id || !!query.id
+    }
+  );
+
+  const { data: commonData } = useQuery(
+    queryKeys.commons.codeDetails({ codeId: 22 }),
+    () => fetchCommonCodeDetails({ codeId: 22 }),
+    {
+      refetchOnMount: true
     }
   );
 
@@ -75,7 +85,9 @@ function OrderSearchDelieryForm({
             id="Smart Delivery Search Frame"
             title="스마트 택배 배송조회"
             style={{ width: '100%', height: 'calc(100% - 40px)' }}
-            src={`https://info.sweettracker.co.kr/tracking/5?t_key=oUmDGhMFamBdY7XDlEcpbQ&t_code=${orderDelivery?.deliveryCode}&t_invoice=${orderDelivery?.contents}`}
+            src={`https://info.sweettracker.co.kr/tracking/5?t_key=${
+              find(commonData, { codeId: 22 })?.description || ''
+            }&t_code=${orderDelivery?.deliveryCode}&t_invoice=${orderDelivery?.contents}`}
           />
         </Box>
       )}
