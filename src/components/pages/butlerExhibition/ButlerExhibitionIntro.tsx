be69e86a0339756635f81de0cad895dd36511066
Noah <kangyongsuek@gmail.com>
2023-05-31 00:06:41 +0900

    [HOTFIX] 버틀러 기획전 페이지 이미지 최적화 적용 (#823)

--- src/components/pages/butlerExhibition/ButlerExhibitionIntro.tsx
+++ src/components/pages/butlerExhibition/ButlerExhibitionIntro.tsx
@@ -1,42 +1,51 @@
 import { Box, Image, Typography } from '@mrcamelhub/camel-ui';
 
+import { getImageResizePath } from '@utils/common';
+
 function ButlerExhibitionIntro() {
   return (
-    <Box
-      component="section"
-      customStyle={{
-        padding: '46px 20px 53px',
-        backgroundImage: `url(https://${process.env.IMAGE_DOMAIN}/assets/images/butler/butler-crinkled-paper-texture.png)`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-      }}
-    >
+    <Box component="section" customStyle={{ position: 'relative' }}>
       <Image
-        src={`https://${process.env.IMAGE_DOMAIN}/assets/images/butler/intro-typo.png`}
-        alt="Intro Typo Img"
+        src={getImageResizePath({
+          imagePath: `https://${process.env.IMAGE_DOMAIN}/assets/images/butler/butler-crinkled-paper-texture.png`,
+          w: 300
+        })}
         disableAspectRatio
+        alt="배경이미지"
       />
-      <Typography
-        variant="h4"
-        customStyle={{
-          marginTop: 10,
-          '& > span': {
-            fontWeight: 700
-          }
-        }}
-      >
-        구하기 힘든 상태좋은 샤넬 가브리엘 백팩 <br />
-        <span>카멜이 다 구매했습니다.</span>
-      </Typography>
-      <Image
-        src={`https://${process.env.IMAGE_DOMAIN}/assets/images/butler/best-item-in-the-world.png`}
-        alt="Best Item Img"
-        disableAspectRatio
-        customStyle={{
-          marginTop: 38
-        }}
-      />
+      <Box customStyle={{ position: 'absolute', top: 0, left: 0, padding: '46px 20px 53px' }}>
+        <Image
+          src={getImageResizePath({
+            imagePath: `https://${process.env.IMAGE_DOMAIN}/assets/images/butler/intro-typo.png`,
+            w: 300
+          })}
+          alt="Intro Typo Img"
+          disableAspectRatio
+        />
+        <Typography
+          variant="h4"
+          customStyle={{
+            marginTop: 10,
+            '& > span': {
+              fontWeight: 700
+            }
+          }}
+        >
+          구하기 힘든 상태좋은 샤넬 가브리엘 백팩 <br />
+          <span>카멜이 다 구매했습니다.</span>
+        </Typography>
+        <Image
+          src={getImageResizePath({
+            imagePath: `https://${process.env.IMAGE_DOMAIN}/assets/images/butler/best-item-in-the-world.png`,
+            w: 300
+          })}
+          alt="Best Item Img"
+          disableAspectRatio
+          customStyle={{
+            marginTop: 38
+          }}
+        />
+      </Box>
     </Box>
   );
 }
