0ba8c5b7602546e42354b806eca817fa3c831f69
kimjh96 <kimjhs@kakao.com>
2023-07-27 00:54:03 +0900

    [HOTFIX] 이벤트 콘텐츠 미노출 및 매물 목록 검색결과가 존재하지 않는 문제 수정 (#952)

--- pages/events/appFirstPayment.tsx
+++ pages/events/appFirstPayment.tsx
@@ -77,94 +77,92 @@ function AppFirstPayment() {
         alt="App First Event Main Img"
         disableAspectRatio
       />
-      {!checkAgent.isMobileApp() && (
-        <Flexbox
-          direction="vertical"
-          gap={52}
-          customStyle={{
-            padding: '73px 32px 53px'
-          }}
-        >
-          <Flexbox direction="vertical" gap={12} alignment="center">
-            <Chip variant="solid" brandColor="black">
-              <Typography
-                weight="bold"
-                color="uiWhite"
-                customStyle={{ textAlign: 'center', width: '100%' }}
-              >
-                이벤트 내용
-              </Typography>
-            </Chip>
-            <Typography weight="bold" variant="h2" customStyle={{ textAlign: 'center' }}>
-              APP 설치 및 가입 후,
-              <br />
-              결제 시 자동으로 할인 적용
+      <Flexbox
+        direction="vertical"
+        gap={52}
+        customStyle={{
+          padding: '73px 32px 53px'
+        }}
+      >
+        <Flexbox direction="vertical" gap={12} alignment="center">
+          <Chip variant="solid" brandColor="black">
+            <Typography
+              weight="bold"
+              color="uiWhite"
+              customStyle={{ textAlign: 'center', width: '100%' }}
+            >
+              이벤트 내용
             </Typography>
-          </Flexbox>
-          <Flexbox direction="vertical" gap={12} alignment="center">
-            <Chip variant="solid" brandColor="black">
-              <Typography
-                weight="bold"
-                textAlign="center"
-                color="uiWhite"
-                customStyle={{ width: '100%' }}
-              >
-                이벤트 기간
-              </Typography>
-            </Chip>
-            <Typography weight="bold" variant="h2" textAlign="center">
-              7월 31일(월)까지
-            </Typography>
-          </Flexbox>
-          <Flexbox direction="vertical" gap={12} alignment="center">
-            <Chip variant="solid" brandColor="black">
-              <Typography
-                weight="bold"
-                color="uiWhite"
-                customStyle={{ textAlign: 'center', width: '100%' }}
-              >
-                혜택
-              </Typography>
-            </Chip>
-            <Typography weight="bold" variant="h2" customStyle={{ textAlign: 'center' }}>
-              안전결제수수료 무료
-            </Typography>
-            <Image
-              src={getImageResizePath({
-                imagePath: `https://${process.env.IMAGE_DOMAIN}/assets/images/events/app-first-payment-event-benefit-2.png`,
-                w: 390
-              })}
-              alt="App First Event Main Img"
-              disableAspectRatio
-              customStyle={{
-                marginTop: 20
-              }}
-            />
-          </Flexbox>
-          <Box customStyle={{ borderTop: '1px solid #DCDDE0', paddingTop: 52 }}>
-            <Typography weight="bold">안내사항</Typography>
-            <InfoList>
-              <li>
-                <Typography variant="h4" color="ui60">
-                  결제가 판매자에 의해 취소된 경우, 본 이벤트가 자동으로 다시 적용됩니다.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="h4" color="ui60">
-                  부정한 방법으로 이벤트에 참여한 경우 결제가 취소되거나 계정이 정지될 수 있습니다.
-                </Typography>
-              </li>
-              <li>
-                <Typography variant="h4" color="ui60">
-                  내부 의사결정 및 상황에 따라 사전 공지 없이 종료될 수 있습니다. 늦지 않게
-                  참여해보세요!
-                </Typography>
-              </li>
-            </InfoList>
-            <Typography />
-          </Box>
+          </Chip>
+          <Typography weight="bold" variant="h2" customStyle={{ textAlign: 'center' }}>
+            APP 설치 및 가입 후,
+            <br />
+            결제 시 자동으로 할인 적용
+          </Typography>
         </Flexbox>
-      )}
+        <Flexbox direction="vertical" gap={12} alignment="center">
+          <Chip variant="solid" brandColor="black">
+            <Typography
+              weight="bold"
+              textAlign="center"
+              color="uiWhite"
+              customStyle={{ width: '100%' }}
+            >
+              이벤트 기간
+            </Typography>
+          </Chip>
+          <Typography weight="bold" variant="h2" textAlign="center">
+            7월 31일(월)까지
+          </Typography>
+        </Flexbox>
+        <Flexbox direction="vertical" gap={12} alignment="center">
+          <Chip variant="solid" brandColor="black">
+            <Typography
+              weight="bold"
+              color="uiWhite"
+              customStyle={{ textAlign: 'center', width: '100%' }}
+            >
+              혜택
+            </Typography>
+          </Chip>
+          <Typography weight="bold" variant="h2" customStyle={{ textAlign: 'center' }}>
+            안전결제수수료 무료
+          </Typography>
+          <Image
+            src={getImageResizePath({
+              imagePath: `https://${process.env.IMAGE_DOMAIN}/assets/images/events/app-first-payment-event-benefit-2.png`,
+              w: 390
+            })}
+            alt="App First Event Main Img"
+            disableAspectRatio
+            customStyle={{
+              marginTop: 20
+            }}
+          />
+        </Flexbox>
+        <Box customStyle={{ borderTop: '1px solid #DCDDE0', paddingTop: 52 }}>
+          <Typography weight="bold">안내사항</Typography>
+          <InfoList>
+            <li>
+              <Typography variant="h4" color="ui60">
+                결제가 판매자에 의해 취소된 경우, 본 이벤트가 자동으로 다시 적용됩니다.
+              </Typography>
+            </li>
+            <li>
+              <Typography variant="h4" color="ui60">
+                부정한 방법으로 이벤트에 참여한 경우 결제가 취소되거나 계정이 정지될 수 있습니다.
+              </Typography>
+            </li>
+            <li>
+              <Typography variant="h4" color="ui60">
+                내부 의사결정 및 상황에 따라 사전 공지 없이 종료될 수 있습니다. 늦지 않게
+                참여해보세요!
+              </Typography>
+            </li>
+          </InfoList>
+          <Typography />
+        </Box>
+      </Flexbox>
     </GeneralTemplate>
   );
 }
